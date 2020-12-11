159f070420e2ba57aafb46028b697d4089e31adf
yangbo <yangbo.yancy@bytedance.com>
2020-12-11 11:52:10 +0800

    feat: 添加实现方案

---
+++ index.js
@@ -0,0 +1,25 @@
+const arr = [
+  {
+    type: 'html',
+    info: {}, // 标签信息
+    children: [
+      {
+        type: 'div1',
+        info: {},
+        children: [
+          {
+            type: 'div2',
+            info: {},
+            children: [],
+          }
+        ]
+      },
+      {
+        type: 'div3',
+        info: {},
+        children: [],
+      }
+    ],
+  },
+  {},
+];
