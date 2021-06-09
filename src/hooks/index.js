b1ea0c472ea5a5e80afdf1a9abe469273f32ef07
yangbo <yangbo.yancy@bytedance.com>
2021-06-09 21:40:14 +0800

    修改问题

--- src/hooks/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.useCompilerHtml = exports.useRotate = void 0;
-var useRotate_1 = require("./useRotate");
-exports.useRotate = useRotate_1.useRotate;
-var useCompilerHtml_1 = require("./useCompilerHtml");
-exports.useCompilerHtml = useCompilerHtml_1.useCompilerHtml;
--- src/hooks/index.ts
+++
@@ -1,7 +0,0 @@
-import { useRotate } from './useRotate';
-import { useCompilerHtml } from './useCompilerHtml';
-
-export {
-  useRotate,
-  useCompilerHtml,
-}
